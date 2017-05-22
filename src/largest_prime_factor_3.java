d1b515496d420fa67212fadd44ba1e156dd637f9
hjnowakowski <hjnowakowski@gmail.com>
2017-05-22 13:56:24 +0200

    classes separated into different files

---
+++ src/largest_prime_factor_3.java
@@ -0,0 +1,14 @@
+/**
+ * Created by henryknowakowski on 22.05.2017.
+ */
+
+//Problem nr 3
+
+class largest_prime_factor_3{
+
+    long limit;
+    largest_prime_factor_3(long a){
+        limit = a;
+    }
+
+}
